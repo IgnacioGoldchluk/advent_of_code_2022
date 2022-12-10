78e284ee30d19835bc2bded56ecd77a885570153
IgnacioGoldchluk <goldchlukignacio@gmail.com>
2022-12-10 19:58:50 -0300

    improve day9

--- src/solutions/day9.rs
+++ src/solutions/day9.rs
@@ -1,7 +1,6 @@
 use std::collections::HashSet;
 use std::fs;
 
-// TODO: Refactor to functional code
 type Point = (i32, i32);
 
 pub fn solution() {
@@ -11,37 +10,47 @@ pub fn solution() {
         .map(|line| line.to_owned())
         .collect();
 
-    let mut rope = vec![(0, 0); 2];
-    println!("{}", solve(&commands, &mut rope));
-    let mut rope = vec![(0, 0); 10];
-    println!("{}", solve(&commands, &mut rope));
+    println!("{}", solve(&commands, 2));
+    println!("{}", solve(&commands, 10));
 }
 
-fn solve(moves: &[String], rope: &mut [Point]) -> usize {
+fn solve(moves: &[String], length: usize) -> usize {
     let mut visited: HashSet<Point> = HashSet::new();
-    for mov in moves {
-        let (x, y, n) = match mov.split_at(2) {
-            ("R ", n) => (1, 0, n),
-            ("L ", n) => (-1, 0, n),
-            ("U ", n) => (0, 1, n),
-            ("D ", n) => (0, -1, n),
-            (_, _) => unreachable!(),
-        };
+    let mut rope = vec![(0, 0); length];
 
-        for _ in 0..n.parse::<usize>().unwrap() {
-            rope[0].0 += x;
-            rope[0].1 += y;
-            for i in 1..rope.len() {
-                if let Some(pos) = move_adjacent(&rope[i], &rope[i - 1]) {
-                    rope[i] = pos;
-                } else {
-                    break;
-                }
-            }
-            visited.insert(*rope.last().unwrap());
+    moves
+        .iter()
+        .flat_map(to_move)
+        .for_each(|movement| apply_movement(movement, &mut rope, &mut visited));
+
+    visited.len()
+}
+
+fn to_move(line: &String) -> Vec<(i32, i32)> {
+    let (dir, repeats) = match line.split_once(' ').unwrap() {
+        ("R", n) => ((1, 0), n),
+        ("L", n) => ((-1, 0), n),
+        ("U", n) => ((0, 1), n),
+        ("D", n) => ((0, -1), n),
+        (_, _) => unreachable!(),
+    };
+    vec![dir; repeats.parse::<usize>().unwrap()]
+}
+
+fn apply_movement(
+    movement: (i32, i32),
+    rope: &mut Vec<(i32, i32)>,
+    seen: &mut HashSet<(i32, i32)>,
+) {
+    rope[0].0 += movement.0;
+    rope[0].1 += movement.1;
+
+    for i in 1..rope.len() {
+        if let Some(new_pos) = move_adjacent(&rope[i], &rope[i - 1]) {
+            rope[i] = new_pos;
         }
     }
-    visited.len()
+    seen.insert(*rope.last().unwrap());
 }
 
 fn move_adjacent(tail: &Point, head: &Point) -> Option<Point> {
@@ -55,6 +64,6 @@ fn move_adjacent(tail: &Point, head: &Point) -> Option<Point> {
     } else if dy == 2 || dy == -2 {
         Some((head.0, head.1 + dy.clamp(-1, 1)))
     } else {
-        None // already adjacent
+        None
     }
 }
