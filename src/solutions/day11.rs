1fa289325600d8321e3820276be1a1e1d1799635
IgnacioGoldchluk <goldchlukignacio@gmail.com>
2022-12-11 14:34:13 -0300

    add gcd to prevent overflow

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -7,9 +7,10 @@ use std::fs;
 struct Monkey {
     pub items: Vec<u64>,
     operation: Box<dyn Fn(u64) -> u64>,
-    test: Box<dyn Fn(u64) -> bool>,
+    test: u64,
     throw: HashMap<bool, usize>,
     inspected: u64,
+    pub gcd: u64,
 }
 
 impl Monkey {
@@ -25,6 +26,7 @@ impl Monkey {
             test,
             throw,
             inspected: 0,
+            gcd: test,
         }
     }
 
@@ -63,8 +65,8 @@ impl Monkey {
             .collect()
     }
 
-    fn parse_test(monkey_info: &str) -> Box<dyn Fn(u64) -> bool> {
-        let divisor = monkey_info
+    fn parse_test(monkey_info: &str) -> u64 {
+        monkey_info
             .lines()
             .filter(|line| line.contains("Test:"))
             .map(|line| {
@@ -74,9 +76,7 @@ impl Monkey {
                     .unwrap()
             })
             .next()
-            .unwrap();
-
-        Box::new(move |number| number % divisor == 0)
+            .unwrap()
     }
 
     fn parse_operation(monkey_info: &str) -> Box<dyn Fn(u64) -> u64> {
@@ -100,16 +100,24 @@ impl Monkey {
         }
     }
 
-    pub fn perform_round(&mut self) -> Vec<(usize, u64)> {
+    pub fn perform_round(&mut self, divisor: u64) -> Vec<(usize, u64)> {
         let mut operations: Vec<(usize, u64)> = Vec::new();
         self.items.iter().for_each(|item| {
-            let worry_level = (self.operation)(*item) / 3;
-            let to_throw = self.throw.get(&(self.test)(worry_level)).unwrap();
-            operations.push((*to_throw, worry_level));
+            let worry_level = (self.operation)(*item) / divisor;
+            let test = worry_level % self.test == 0;
+            let to_throw = self.throw.get(&test).unwrap();
+            operations.push((*to_throw, worry_level % self.gcd));
             self.inspected += 1;
         });
-        self.items = vec![];
+        self.items.clear();
         operations
+    }
+}
+
+fn assign_gcd(monkeys: &mut Vec<Monkey>) {
+    let gcd: u64 = monkeys.iter().map(|monkey| monkey.gcd).product();
+    for monkey in monkeys.iter_mut() {
+        monkey.gcd = gcd;
     }
 }
 
@@ -120,10 +128,12 @@ pub fn solution() {
         .map(Monkey::new)
         .collect();
 
+    assign_gcd(&mut monkeys);
+
     for _round in 0..20 {
         for idx in 0..monkeys.len() {
             let monkey = &mut monkeys[idx];
-            let pushes = monkey.perform_round();
+            let pushes = monkey.perform_round(3);
             for (target, val) in pushes.iter() {
                 let other_monkey = &mut monkeys[*target];
                 other_monkey.items.push(*val);
